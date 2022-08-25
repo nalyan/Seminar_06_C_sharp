36bfd8441dfd48211cf70bf36f149299149ec9d8
ilnar <ilnar-sov@mail.ru>
2022-08-25 22:47:10 +0300

    Задача 2

---
+++ Practical tasks/task_02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int m = InputNumber("Введите количество строк: ");
+int n = InputNumber("Введите количество столбцов: ");
+int[,] matrix = new int[m, n];
+for(int i = 0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = i+j;
+    }
+}
+PrintMatrix(matrix);
